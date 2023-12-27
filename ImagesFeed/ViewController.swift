438ac25056dfdd000c948698d01640cb54b10a02
Art Hack <art.hack.ios@gmail.com>
2023-12-27 22:51:41 +0500

    Initial Commit

---
+++ ImagesFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImagesFeed
+//
+//  Created by arthack on 27.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
